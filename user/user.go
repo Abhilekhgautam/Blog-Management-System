8145bd99534e58e712b0480df6893509a4f12361
abhilekh <abhilekhgautam119@gmail.com>
2022-10-25 12:36:19 +0545

    signup get and post route created

--- user/user.go
+++ user/user.go
@@ -16,15 +16,26 @@ import (
 	"net/http"
 	"projectOne/blog/db"
 	"time"
+	"golang.org/x/crypto/bcrypt"
+	"projectOne/blog/Services"
 )
 
-var templates = template.Must(template.ParseFiles("./templates/login.html"))
+var templates = template.Must(template.ParseFiles("./templates/login.html","./templates/signup.html"))
 
 var characters = []rune("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-// RandStr ... generates random string for cookie value.
-func RandStr() string {
-	str := make([]rune, 60)
+func HashPassword(password string) string{
+    hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil{
+		log.Fatal(err)
+	}
+		return string(hashedPass)
+}
+
+// RandStr ... generates random string
+func RandStr(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	str := make([]rune, length)
 	for i := range str {
 		str[i] = characters[rand.Intn(len(characters))]
 	}
@@ -39,10 +50,11 @@ type Secret struct {
 
 // this denotes the actual user information
 type User struct{
-	Username string  `json:username`
-	Email    string  `json:email`
-	Password string  `json:password`
-    BlogTitle string `json:blogTitle`
+	UserId   int64   `json:"userid"`
+	Username string  `json:"username"`
+	Email    string  `json:"email"`
+	Password string  `json:"password"`
+    BlogTitle string `json:"blogTitle"`
 }
 // Get signup form for user.
 // this is a get request from the user.
@@ -50,6 +62,77 @@ func GetSignup(w http.ResponseWriter, r *http.Request){
 	err := templates.ExecuteTemplate(w,"signup.html", nil)
 	if err != nil{
 		log.Fatal("Unable to render provided template")
+	}
+}
+
+// accepts post request for creating a new user.
+func Signup(w http.ResponseWriter, r *http.Request){
+	err := r.ParseForm()
+	if err != nil{
+		log.Fatal("Unable to parse the form data")
+	}
+	username := r.PostForm.Get("username")
+	email := r.PostForm.Get("email")
+	password := r.PostForm.Get("password")
+
+	if len(username) == 0 || len(email) == 0 || len(password) == 0 {
+		errMsg := "All the fields are required"
+		err := templates.ExecuteTemplate(w,"signup.html", errMsg)
+		if err != nil{
+			log.Fatal("Unable to render signup template")
+		}
+	}
+
+	db.ConnectDB()
+	defer db.Db.Close()
+	row := db.Db.QueryRow("Select username from Users where username = ?",username)
+	if err := row.Scan(); err != nil{
+		if err == sql.ErrNoRows{
+			row := db.Db.QueryRow("Select email from emails where email = ?", email)
+			if err := row.Scan(); err != nil{
+				if err == sql.ErrNoRows{
+					hashedPass := HashPassword(password)
+					_, err = db.Db.Exec("Insert into User( username, email, password, blogTitle) values(?,?,?,NULL)", username, email, hashedPass)
+					if err != nil {
+						 log.Fatal("Unable to insert data to database", err)
+					} else {
+						  generated_token := RandStr(24)
+						  current_token := time.Now().Unix()
+						  user_id := 0
+						  row := db.Db.QueryRow("Select user_id from User where username = ?",username)
+						  if row.Scan(&user_id); err != nil{
+							log.Fatal("Unable to scan data")
+						  }
+						  mail.SendEmail(username, generated_token, email)
+						  err := templates.ExecuteTemplate(w,"signup.html","A confirmation email has been sent")
+						  if err != nil{
+							log.Fatal("Unable to render the signup template")
+						  }
+						  _, err = db.Db.Exec("Insert into Token(token,date_generated, date_expires, date_used, user_id) values(?,?,?,NULL,?)",generated_token,current_token, current_token + 172800,user_id)
+						  if err != nil{
+							  log.Fatal("Unable to insert token data into database:", err)
+						  }
+					 } 
+				} else{
+					log.Fatal("Something went wrong:", err)
+				}
+			} else{
+				err := templates.ExecuteTemplate(w,"signup.html","this email already exists")
+				if err != nil{
+				  log.Fatal("Unable to render the signup template")
+				}
+			}
+		} else{
+			err := templates.ExecuteTemplate(w,"signup.html","this email already exists")
+				if err != nil{
+				  log.Fatal("Unable to render the signup template")
+				}
+		}
+	} else{
+		err := templates.ExecuteTemplate(w,"signup.html","this usename is already takaen")
+				if err != nil{
+				  log.Fatal("Unable to render the signup template")
+				}
 	}
 }
 
@@ -103,7 +186,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		if err != nil || cookie == nil {
 			rand.Seed(time.Now().UnixNano())
 			// store random string value
-			randString := RandStr()
+			randString := RandStr(60)
 			fmt.Println("Cookie value:", randString)
 
 			http.SetCookie(w, &http.Cookie{
@@ -124,9 +207,9 @@ func Login(w http.ResponseWriter, r *http.Request) {
 			http.Redirect(w, r, "/admin", 302)
 			return
 		}
-		rand.Seed(time.Now().UnixNano())
+
 		// store random string value
-		randString := RandStr()
+		randString := RandStr(60)
 		fmt.Println("Cookie value:", randString)
 
 		http.SetCookie(w, &http.Cookie{
