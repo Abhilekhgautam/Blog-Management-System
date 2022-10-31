663e0191031ea1fe9de8553a7f05a13ff76a83c0
abhilekh <abhilekhgautam119@gmail.com>
2022-10-31 12:36:52 +0545

    use dotenv files for app configuration

--- db/db.go
+++ db/db.go
@@ -6,23 +6,28 @@ import (
 	"github.com/go-sql-driver/mysql"
 	"log"
 	"os"
+	"github.com/joho/godotenv"
 )
 
 var Db *sql.DB
 
+
 //ConnectDB - connect the database with provided configuration
 func ConnectDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file:",err)
+	  }
 	// configuration for mysql..
 	cfg := mysql.Config{
-		User:                 os.Getenv("User"),
-		Passwd:               os.Getenv("Pass"),
+		User:                 os.Getenv("DB_USER"),
+		Passwd:               os.Getenv("DB_PASS"),
 		Net:                  "tcp",
-		Addr:                 "127.0.0.1:3306",
-		DBName:               "blog",
+		Addr:                 os.Getenv("DB_ADDRESS"),
+		DBName:               os.Getenv("DB_NAME"),
 		AllowNativePasswords: true,
 	}
 
-	var err error
 	// open  a database handle.
 	Db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
