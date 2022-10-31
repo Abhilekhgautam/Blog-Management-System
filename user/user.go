5c71ffed49440ff2644b1b85e9317139ec122180
abhilekh <abhilekhgautam119@gmail.com>
2022-10-31 11:56:22 +0545

    forgot password functionality added

--- user/user.go
+++ user/user.go
@@ -23,7 +23,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-var templates = template.Must(template.ParseFiles("./templates/login.html","./templates/signup.html","./templates/chose-title.html"))
+var templates = template.Must(template.ParseFiles("./templates/login.html","./templates/signup.html","./templates/chose-title.html","./templates/email-prompt.html","./templates/new-password.html"))
 
 var characters = []rune("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
@@ -45,8 +45,8 @@ func RandStr(length int) string {
 	return string(str)
 }
 
-
-var Store = sessions.NewCookieStore([]byte(RandStr(30)))
+//todo use env variable for the key
+var Store = sessions.NewCookieStore([]byte("VqM5LNd8BhUFnI4P8oIWEqf6oiPIASM4TV7RilpGtgUkKmnyIiKPnEC43aut"))
 
 // this denotes the secret datas related to a user.
 type Secret struct {
@@ -68,27 +68,39 @@ type User struct{
 // Get signup form for user.
 // this is a get request from the user.
 func GetSignup(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Trying to get signup page for you....")
 	session, err := Store.Get(r, "smart-blogger-cookie")
 	if err != nil{
 		log.Fatal("Unable to retrieve session:",err)
 	}
+	fmt.Println("Trying to get signup page for you....")
+
 	if session.IsNew{
+		fmt.Println("New Session....")
 		err := templates.ExecuteTemplate(w,"signup.html", nil)
 		if err != nil{
 			log.Fatal("Unable to render provided template")
 		}
 	} else{
 		var username string
-	    err = db.Db.QueryRow("select username from session where password = ?", session.Values["Value"].(string)).Scan(&username)
+		fmt.Println("Trying to get signup page for you....")
+		if len(session.Values) != 4{
+			fmt.Println("len(session.Values) =",len(session.Values))
+			fmt.Println("session.Values =", session.Values["value"].(string))
+		}
+	    err = db.Db.QueryRow("select username from session where password = ?", session.Values["value"].(string)).Scan(&username)
 	    if err != nil{
 		    if err == sql.ErrNoRows{
-			    err = templates.ExecuteTemplate(w, "login.html", nil)
+			    err = templates.ExecuteTemplate(w, "signup.html", nil)
 			        if err != nil {
 				        log.Fatal("Unable to render provided template")
 			           }
-		            }
+		            } else{
+						log.Fatal("Something went wrong:", err)
+					}
 	    } else{
-			http.Redirect(w,r, "/" + session.Values["blogTitle"].(string) + "/admin", http.StatusFound)
+			blogTitle := session.Values["blogTitle"].(string)
+			http.Redirect(w,r, "/" + blogTitle + "/admin/", http.StatusFound)
 		}
 	}
 }
@@ -132,7 +144,7 @@ func Signup(w http.ResponseWriter, r *http.Request){
 						  if row.Scan(&user_id); err != nil{
 							log.Fatal("Unable to scan data")
 						  }
-						  mail.SendEmail(username, generated_token, email)
+						  mail.SendEmail(username, generated_token, email,"confirmation")
 						  err := templates.ExecuteTemplate(w,"signup.html","A confirmation email has been sent")
 						  if err != nil{
 							log.Fatal("Unable to render the signup template")
@@ -186,15 +198,29 @@ func GetLogin(w http.ResponseWriter, r *http.Request) {
 	if err != nil{
 		if err == sql.ErrNoRows{
 			err = templates.ExecuteTemplate(w, "login.html", nil)
-			if err != nil {
-				log.Fatal("Unable to render provided template")
-			}
+		    if err != nil {
+			  log.Fatal("Unable to render provided template")
+		    }
 			return
 		}
 	}
 	// user is already logged in so take him to admin home page
-	// todo: change the link to title/admin
-	path := "/" + session.Values["blogTitle"].(string) + "/admin"
+    // but check if title is empty..
+	title := session.Values["blogTitle"].(string)
+	if len(title) < 2{
+        err = templates.ExecuteTemplate(w, "chose-title.html", struct {
+			Username string 
+			Msg string
+			}{
+			Username: username,
+			Msg: "",
+			})
+		if err != nil {
+			log.Fatal("Unable to render provided template:",err)
+		   }
+		return  
+	}
+	path := "/" + title + "/admin/"
 	http.Redirect(w, r, path, http.StatusFound)
 }
 
@@ -268,12 +294,19 @@ func Login(w http.ResponseWriter, r *http.Request) {
 						   }
 						return  
 					}
-					http.Redirect(w, r, "/" + session.Values["blogTitle"].(string) + "/admin", http.StatusFound)
+					http.Redirect(w, r, "/" + session.Values["blogTitle"].(string) + "/admin/", http.StatusFound)
 				} else{
 					var username string
 					err = db.Db.QueryRow("select username from session where password = ?", session.Values["value"].(string)).Scan(&username)
 					if err != nil{
 					  if err == sql.ErrNoRows{
+						fmt.Println("NO such sessions found:",err)
+						fmt.Println(session.Values["value"].(string))
+						session.Options.MaxAge = -1
+						err := sessions.Save(r, w)
+						if err != nil{
+							log.Fatal(err)
+						}
 					   http.Redirect(w, r, "/login", http.StatusFound)
 						}
 					} else{
@@ -290,7 +323,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 							   }
 							return  
 						}
-						http.Redirect(w,r, "/" + session.Values["blogTitle"].(string) + "/admin", http.StatusFound)
+						http.Redirect(w,r, "/" + session.Values["blogTitle"].(string) + "/admin/", http.StatusFound)
 					}
 				}
 			} else{
@@ -306,14 +339,21 @@ func LogOut(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("Unable to get session value:", err)
 	}
 	db.ConnectDB()
-	_, err = db.Db.Query("delete from session where password = ?", session.Values["Value"].(string))
+	_, err = db.Db.Query("delete from session where password = ?", session.Values["value"].(string))
 	if err != nil {
 		http.Redirect(w, r, "/login", http.StatusFound)
 		return
 	}
+	// setting Maxage < 0 deletes the session..
+	session.Options.MaxAge = -1
+	err = sessions.Save(r, w)
+	if err != nil{
+		log.Fatal(err)
+	}
 	http.Redirect(w, r, "/login", http.StatusFound)
 }
 
+// prompts user to set title..
 func SetTitle(w http.ResponseWriter, r *http.Request){
 	err := r.ParseForm()
 	if err != nil {
@@ -342,7 +382,135 @@ func SetTitle(w http.ResponseWriter, r *http.Request){
 		} else {
 			blogTitle := strings.Replace(title, " ", "-", -1)
 			session.Values["blogTitle"] = blogTitle
-			http.Redirect(w, r, "/" + blogTitle + "/admin", http.StatusFound)
+			err = session.Save(r, w)
+			if err != nil{
+				log.Fatal("Could not save session value")
+			}
+			http.Redirect(w, r, "/" + blogTitle + "/admin/", http.StatusFound)
 		}
 	}
+}
+
+// prompt to enter new password for user....
+func GetChangePassword(w http.ResponseWriter, r *http.Request){
+	err := r.ParseForm()
+	if err != nil{
+		log.Fatal("Unable to parse data")
+	}
+	token := r.Form.Get("token")
+	
+	err = templates.ExecuteTemplate(w,"new-password.html", struct{
+		Token string
+		Msg   string
+	}{
+		Token: token,
+		Msg: " ",
+	})
+	if err != nil{
+		log.Fatal("Unable to render provided template")
+	}
+
+}
+// change password changes password ...
+func ChangePassword(w http.ResponseWriter, r *http.Request){
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal("Unable to Parse Form")
+	}
+	token := r.Form.Get("token")
+
+	pass := r.PostForm.Get("password")
+	current_time := time.Now().Unix()
+
+	var user_id int
+	var date_generated int64
+	var date_expires  int64
+	var date_used     int64
+	row := db.Db.QueryRow("Select user_id, date_generated, date_expires, date_used from Token where token = ?", token)
+	if err := row.Scan(&user_id, &date_generated, &date_expires, &date_used); err != nil{
+       if err == sql.ErrNoRows{
+		//todo: no such token provide a link to signup..
+		http.Redirect(w, r, "/signup", http.StatusFound)
+		fmt.Println("No such rows..")
+	   } else {
+		log.Fatal("Something went wrong:", err)
+	   }
+	}
+
+   //reuse of the token...
+	if (date_used != 0){
+       http.Redirect(w,r, "/signup", http.StatusFound)
+	}
+	// use of expired token...
+	if(date_expires < current_time){
+		//todo: inform about the expired token and prompt for re confirmation..
+		fmt.Println("Token expired..")
+
+	} else{
+		_, err = db.Db.Exec("Update Token set date_used = ? where token=?",current_time, token)
+	    if err != nil {
+		  log.Fatal("Unable to update with given data")
+	     }
+         hashed_password := HashPassword(pass)
+		_, err = db.Db.Exec("Update User set password = ? where user_id=?", hashed_password, user_id)
+	    if err != nil {
+		  log.Fatal("Unable to update with given data")
+	     } else {
+		   http.Redirect(w, r, "/login", http.StatusFound)
+	      }
+	}
+}
+
+// prompts user to enter email...
+func GetForgot(w http.ResponseWriter, r *http.Request){
+	err := templates.ExecuteTemplate(w,"email-prompt.html", struct{
+		Link string
+		Msg  string
+	}{
+		Link: "forgot",
+		Msg : "",
+	})
+		if err != nil{
+			log.Fatal("Unable to render provided template:",err)
+		}
+}
+
+// post request for change in password...
+func ForgotPassword(w http.ResponseWriter, r *http.Request){
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal("Unable to Parse Form")
+	}
+	email := r.PostForm.Get("email")
+	fmt.Println("Email Address:",email)
+    var user_id string
+	db.ConnectDB()
+	row := db.Db.QueryRow("Select user_id from User where email = ?", email)
+	if row.Scan(&user_id); err != nil{
+		if err == sql.ErrNoRows{
+			//.. no such email found.
+		} else{
+			log.Fatal("Unable to scan data")
+		}
+	}
+    fmt.Println("The Id for the given email is:",user_id)
+	generated_token := RandStr(24)
+	current_token := time.Now().Unix()
+
+	_, err = db.Db.Exec("Insert into Token(token,date_generated, date_expires, date_used, user_id) values(?,?,?,0,?)",generated_token,current_token, current_token + 172800,user_id)
+	if err != nil{
+		log.Fatal("Unable to insert token data into database:", err)
+	}
+
+	mail.SendEmail("",generated_token,email,"forgot")
+	err = templates.ExecuteTemplate(w,"email-prompt.html", struct{
+		Link string
+		Msg  string
+	}{
+		Link: "forgot",
+		Msg : "A confirmation email has been sent.",
+	})
+		if err != nil{
+			log.Fatal("Unable to render provided template")
+		}
 }
